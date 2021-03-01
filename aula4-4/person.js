707cfd9e96aa415457a79a1efe4dab90e919df51
Michelle <mls.srbek@gmail.com>
2021-03-01 20:44:23 -0300

    Added person file and some changes

---
+++ aula4-4/person.js
@@ -0,0 +1,15 @@
+let person = {
+    name: 'Michelle',
+    age: 26
+};
+
+person.address = {
+    street: 'xablau',
+    number:  '42',
+    city: 'Isengard'
+};
+
+person['favFood'] = ['Icecream'];
+person['nicknames'] = ['Mi', 'MisChief', 'Paprika'];
+
+console.log(person);
