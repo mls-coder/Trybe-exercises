18d75302d22a8369bb60833f7bdb77773ddd032d
Michelle <mls.srbek@gmail.com>
2021-03-07 15:41:33 -0300

    lecture 5.1 part 2 from course

---
+++ aula5-1/courseP2.js
@@ -0,0 +1,7 @@
+//retorna toda a tag
+let getId = document.getElementById('tittle');
+console.log(getId);
+
+//retorna somente o conteúdo 
+let getInner = document.getElementById('tittle').innerHTML;
+console.log(getInner);
