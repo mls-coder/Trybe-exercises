b5236fdf92987384fdbd1b2aa040162a95f9d7f9
Michelle <mls.srbek@gmail.com>
2021-03-07 15:57:17 -0300

    manipulationg content with getElementById

--- aula5-1/courseP2.js
+++ aula5-1/courseP2.js
@@ -5,3 +5,11 @@ console.log(getId);
 //retorna somente o conteúdo 
 let getInner = document.getElementById('tittle').innerHTML;
 console.log(getInner);
+
+//retorna tag dentro da tag selecionada
+let getInnerTag = document.getElementById('helloWorld').innerHTML;
+console.log(getInnerTag);
+
+//retorna somente o texto filtrado
+let getInnerText = document.getElementById('helloWorld').innerText;
+console.log(getInnerText);
