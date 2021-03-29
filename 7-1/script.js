86416c018c984b94c93326e65682253f8b326a31
Michelle <mls.srbek@gmail.com>
2021-03-29 18:15:05 -0300

    exercise 1 part 1 from course

--- 7-1/script.js
+++ 7-1/script.js
@@ -25,6 +25,18 @@ function testingScope(escopo) {
       console.log(elseScope);
     }
     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-  }
+}
 
   testingScope(true);
+
+const testScope = (escopo) => {
+    if (escopo) {
+        const ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+        console.log(`${ifScope} ótimo, fui utilizada no escopo !`);
+    } else {
+        const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        console.log(`${elseScope} ótimo, fui utilizada no escopo !`);
+    }
+}
+
+testScope(false);
