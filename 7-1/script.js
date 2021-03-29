5b4a341928e3369e34b9717419cec54dc76be47c
Michelle <mls.srbek@gmail.com>
2021-03-29 17:55:39 -0300

    first commit, arrow functions

---
+++ 7-1/script.js
@@ -0,0 +1,30 @@
+
+// Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+//calcule a area do triangulo retangulo
+
+function calcArea(base, height) {
+    return (base * height) / 2;
+}
+console.log(calcArea(8,4));
+
+const area = (base, height) => (base * height) / 2;
+console.log(area(8,4));
+
+// Modifique a estrutura da função para que ela seja uma arrow function .
+// Modifique as concatenações para template literals .
+// Copie o código abaixo.
+
+
+function testingScope(escopo) {
+    if (escopo === true) {
+      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      console.log(ifScope);
+    } else {
+      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  }
+
+  testingScope(true);
