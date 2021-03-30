88458b4bc87e6090bc3681b88a6646568f369b4d
Michelle <mls.srbek@gmail.com>
2021-03-30 17:54:40 -0300

    first commit lecture 7.2

---
+++ aula7-2/script.js
@@ -0,0 +1,14 @@
+const Mi = {
+    name: 'Michelle',
+    lastName: 'Srbek',
+    age: 26,
+    lovesChocolate: true,
+    favShow: {
+        firstPlace: 'The Lord of The rings',
+        secondPlace: 'CCS',
+    }
+};
+
+console.table(Mi);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
