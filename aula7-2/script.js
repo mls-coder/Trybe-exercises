3c11fefdf7a482e630c02c5eed146a3bc2bdd003
Michelle <mls.srbek@gmail.com>
2021-03-30 20:32:25 -0300

    some code from live lecture

--- aula7-2/script.js
+++ aula7-2/script.js
@@ -1,6 +1,7 @@
-const Mi = {
+const person = {
     name: 'Michelle',
     lastName: 'Srbek',
+    nickName: 'Mi',
     age: 26,
     lovesChocolate: true,
     favShow: {
@@ -9,6 +10,15 @@ const Mi = {
     }
 };
 
-console.table(Mi);
+// console.table(person);
+console.log(`Olá, ${person.nickName}! Como vai você?`);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+person.favColor = 'black';
+// console.table(person);
+
+console.log(Object.keys(person));
+console.log(Object.values(person));
+
+console.log(`Existe cor favorita? ${Object.keys(person).includes('favColor')}`);
