10321a56cffb08eb77a61eb45176198d81a7030b
Michelle <mls.srbek@gmail.com>
2021-03-30 21:05:46 -0300

    some code from live lecture

--- aula7-2/script.js
+++ aula7-2/script.js
@@ -6,19 +6,38 @@ const person = {
     lovesChocolate: true,
     favShow: {
         firstPlace: 'The Lord of The rings',
-        secondPlace: 'CCS',
+        secondPlace: 'CCS'
     }
 };
 
 // console.table(person);
 console.log(`Olá, ${person.nickName}! Como vai você?`);
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
 person.favColor = 'black';
 // console.table(person);
 
-console.log(Object.keys(person));
-console.log(Object.values(person));
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
 
 console.log(`Existe cor favorita? ${Object.keys(person).includes('favColor')}`);
+console.log(`Person values: ${Object.values(person)}`);
+console.log(`Persona keys: ${Object.keys(person)}`);
+console.log(`Person entries: ${Object.entries(person)}`);
+
+// console.log(Object.keys(person.favShow));
+
+const books = {
+    book1: "The Hitchhiker's Guide to the Galaxy",
+    book2: "The Restaurant at the End of the Universe",
+    book3: "Life, the Universe and Everything",
+    book4: "So Long, and Thanks for All the Fish",
+    book5: "Mostly Harmless",
+    book6: "And Another Thing..."
+};
+
+const author = {
+    aurhor: 'Douglas Adams'
+};
+
+Object.assign(books, author);
+console.log(books); //books recebeu as entradas de author
