49a9a446dbf67546821441a727b56d1f5155cdad
Michelle <mls.srbek@gmail.com>
2021-03-30 22:10:23 -0300

    exercise 1 part 2 from course

--- aula7-2/script.js
+++ aula7-2/script.js
@@ -48,7 +48,7 @@ Object.assign(booksAuthor, author);
 console.log(booksAuthor);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+//              PART 1
 
 
 
@@ -115,3 +115,36 @@ const order = {
   
   orderModifier(order);
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//     PART 2
+
+
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+};
+
+const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+};
+
+const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+};
+
+// Crie uma função para adicionar o turno da manhã na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela. 
+const modLesson2 = (lesson, turno, value) => {
+    lesson.turno = value;
+}
+
+modLesson2(lesson2, 'turno', 'manhã');
+console.log(lesson2);
+
+//Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro. 
