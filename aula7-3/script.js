d6f860e730ae8c98fd69adf5b3af48761a7fbf2b
Michelle <mls.srbek@gmail.com>
2021-04-01 21:02:06 -0300

    exercise 5 from course

--- aula7-3/script.js
+++ aula7-3/script.js
@@ -166,3 +166,28 @@ assert.strictEqual(myFizzBuzz(9), 'fizz');
 assert.strictEqual(myFizzBuzz(20), 'buzz');
 assert.strictEqual(myFizzBuzz(7), 7);
 assert.strictEqual(myFizzBuzz('25'), false);
+
+
+
+// Compare dois objetos (JSON) para verificar se são idênticos ou não
+
+const assert = require('assert');
+
+const obj1 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  description: 'My Description',
+  title: 'My Title',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
+
+assert.deepStrictEqual(obj1, obj2);
+assert.notDeepStrictEqual(obj1, obj3);
+assert.notDeepStrictEqual(obj2, obj3);
