c444fdcb92d00714c24e5b01fcba3d335b2929c1
Michelle <mls.srbek@gmail.com>
2021-04-01 21:54:09 -0300

    exercise 4 part 2 from course

--- aula7-3/part2.js
+++ aula7-3/part2.js
@@ -58,3 +58,29 @@ assert.strictEqual(output, expected);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+const assert = require('assert');
+// escreva a função findTheNeedle aqui
+const findTheNeedle = (arr, value) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) return parseInt(i);
+    }
+    return -1;
+}
+
+let words = ['house', 'train', 'slide', 'needle', 'book'];
+let expected = 3;
+let output = findTheNeedle(words, 'needle');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = 0;
+output = findTheNeedle(words, 'plant');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = -1;
+output = findTheNeedle(words, 'plat');
+assert.strictEqual(output, expected);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
