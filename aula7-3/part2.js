e29e86a47f23b540de0aa8aac996f3e6e8b1d5c5
Michelle <mls.srbek@gmail.com>
2021-04-02 16:11:24 -0300

    exercise 4 part 3 from course

--- aula7-3/part2.js
+++ aula7-3/part2.js
@@ -154,3 +154,21 @@ const result = [32, 21];
 assert.deepStrictEqual(greaterThanTen(parameter), result);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function secondThirdSmallest(array) {
+    let results = [];
+    array.sort((x, y) => {
+        return x - y;
+    });
+    results = [array[1], array[2]];
+    return results;
+};
+
+const assert = require('assert');
+assert.strictEqual(typeof(secondThirdSmallest), 'function');
+const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
+const result = [5, 6];
+assert.deepStrictEqual(secondThirdSmallest(parameter), result);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
