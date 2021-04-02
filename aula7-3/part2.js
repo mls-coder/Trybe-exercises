e9c4811ebadc915f355de96ffff19cd6e0f7835d
Michelle <mls.srbek@gmail.com>
2021-04-02 16:03:34 -0300

    exercise 3 part 3 from course

--- aula7-3/part2.js
+++ aula7-3/part2.js
@@ -119,20 +119,38 @@ const removeVowels = word => {
         characters[index] === 'i' ||
         characters[index] === 'e' ||
         characters[index] === 'u'
-      ) {
+        ) {
         results += vowel;
         vowel++;
-      } else {
+        } else {
         results += characters[index];
-      }
+        }
     }
     return results;
-  };
+};
   
-  const assert = require('assert');
-  assert.strictEqual(typeof(removeVowels), 'function');
-  const parameter = 'Dayane';
-  const result = 'D1y2n3';
-  assert.strictEqual(removeVowels(parameter), result);
+const assert = require('assert');
+assert.strictEqual(typeof(removeVowels), 'function');
+const parameter = 'Dayane';
+const result = 'D1y2n3';
+assert.strictEqual(removeVowels(parameter), result);
 
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const greaterThanTen = array => {
+    let results = [];
+    for (let index = 0; index < array.length; index += 1) {
+        if (array[index] > 10) {
+        results.push(array[index]);
+        }
+    }
+    return results;
+};
+
+const assert = require('assert');
+assert.strictEqual(typeof(greaterThanTen), 'function');
+const parameter = [4, 10, 32, 9, 21];
+const result = [32, 21];
+assert.deepStrictEqual(greaterThanTen(parameter), result);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
