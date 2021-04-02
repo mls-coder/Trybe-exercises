1684a990cb7ec99b8844cc0f4991b707809e6bf2
Michelle <mls.srbek@gmail.com>
2021-04-01 21:38:23 -0300

    exercise 2 and 3 part 2 from course

--- aula7-3/part2.js
+++ aula7-3/part2.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-
+// escreva a função addOne aqui
 const addOne = arr => {
     let newArr = [];
     for (let i = 0; i < arr.length; i++) {
@@ -16,3 +16,45 @@ const output = addOne(myArray);
 assert.strictEqual(typeof(addOne), 'function');
 assert.deepStrictEqual(output, expected);
 assert.deepStrictEqual(myArray, unchanged);
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const assert = require('assert');
+// escreva a função wordLengths aqui
+const wordLengths = arr => {
+    const lengthArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        lengthArr.push(arr[i].length);
+    }
+    return lengthArr;
+}
+
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+const expected = [3, 6, 10, 5];
+
+assert.strictEqual(typeof(wordLengths), 'function');
+const output = wordLengths(words);
+assert.deepStrictEqual(output, expected);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const assert = require('assert');
+// escreva a função sumAllNumbers aqui
+const sumAllNumbers = arr => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+const numbers = [9, 23, 10, 3, 8];
+const expected = 53;
+const output = sumAllNumbers(numbers);
+
+assert.strictEqual(typeof sumAllNumbers, 'function');
+assert.strictEqual(output, expected);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
