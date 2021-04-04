68841b2dd2d929a23515a4c3a535c03093dd3b11
Michelle <mls.srbek@gmail.com>
2021-04-04 14:50:53 -0300

    calendar files changes, holiday btn

--- aula5-2/script.js
+++ aula5-2/script.js
@@ -16,8 +16,16 @@ const month = () => {
     for (let i = 1; i <= 30; i++) {
         let day = document.createElement('li');
         day.innerHTML = [i];
+        if (i === 4 || i === 22) day.className = 'holiday';
         month.appendChild(day);
     }
 };
 month();
 
+document.querySelector('.btn-holiday').addEventListener('click', () => {
+    const holidays = document.querySelectorAll('.holiday');
+    for (let i = 0; i < holidays.length; i++) {
+        holidays[i].innerHTML = 'Yay Holiday';
+        holidays[i].style.backgroundColor = 'rgb(253,253,150)'; 
+    }
+});
