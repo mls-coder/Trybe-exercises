468f0410314364c543c42bc7ffb7280637439770
Michelle <mls.srbek@gmail.com>
2021-04-05 21:11:08 -0300

    forEach code from live lecture

--- aula8-2/forEach.js
+++ aula8-2/forEach.js
@@ -3,6 +3,7 @@ const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 numbers.forEach((num) => {
     console.log(num);
 });
+
 // cria novo array a partir de numbers
 const numbers3 = numbers => {
     const num3 = [];
@@ -12,3 +13,15 @@ const numbers3 = numbers => {
 
 console.log(numbers3(numbers));
 
+//criando o forEach
+const forE = (arr, action) => {
+    for (let i = 0; i < arr.length; i++) {
+        action(arr[i], i, arr);
+    }
+};
+
+const mutiply3 = num => console.log(num * 3);
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+forE(numbers, mutiply3);
