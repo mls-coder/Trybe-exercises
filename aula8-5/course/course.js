df9d1f99e46757fa149998bb916476507de2ba1c
Michelle <mls.srbek@gmail.com>
2021-04-12 04:26:07 -0300

    exercise 2 from course

--- aula8-5/course/course.js
+++ aula8-5/course/course.js
@@ -1,4 +1,4 @@
-//Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2 , 15 e 54 .
+// Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2 , 15 e 54 .
 
 
 const assert = require('assert');
@@ -18,3 +18,18 @@ rectangles.forEach((rectangle) => {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos. Ou seja:
+
+
+const assert = require('assert');
+
+const sum = (...nums) => nums.reduce((acc, cur) => acc + cur, 0);
+console.log(sum());
+
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
