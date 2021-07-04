cf3c542bb79b8f337626253a6813f9b29c4479b2
Michelle <mls.srbek@gmail.com>
2021-07-04 18:04:08 -0300

    creating actions - live lecture T09

--- front-end/aula16-1/basicExampleT09/index.js
+++ front-end/aula16-1/basicExampleT09/index.js
@@ -1,12 +1,49 @@
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
 const reducer = (state = 0, action) => {
     switch (action.type) {
-        case '': 
-            return ;
-        case '':
-            return ;
-        default: 
-            return ;
+        case INCREMENT: 
+            return state + 1;
+        case DECREMENT:
+            return state - 1;
+        default:
+            return state;
     }
+}
+
+const actionIncrement = () => {
+    const btn = document.getElementById('increase');
+    btn.addEventListener('click', () => {
+        const action = { type: INCREMENT }
+        store.dispatch(action);
+        console.log('increment');
+    });
+};
+
+const actionDecrement = () => {
+    const btn = document.getElementById('decrease');
+    btn.addEventListener('click', () => {
+        const action = { type: DECREMENT }
+        store.dispatch(action);
+        console.log('decrement');
+
+    });
+};
+
+const actionIncrementBy10 = () => {
+    const btn = document.getElementById('increaseBy10');
+    btn.addEventListener('click', () => {
+        const action = { type:  }
+        store.dispatch(action);
+        console.log('increment10');
+    });
+};
+
+window.onload = () => {
+    actionIncrement();
+    actionDecrement();
+    actionIncrementBy10();
 }
 
 const store = Redux.createStore(reducer);
