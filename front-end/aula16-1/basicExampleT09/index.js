d2ba71e53330ff5aa5170292ae8531e57c1f67c1
Michelle <mls.srbek@gmail.com>
2021-07-04 17:10:40 -0300

    creating reducer and store - live lecture T09

---
+++ front-end/aula16-1/basicExampleT09/index.js
@@ -0,0 +1,12 @@
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case '': 
+            return ;
+        case '':
+            return ;
+        default: 
+            return ;
+    }
+}
+
+const store = Redux.createStore(reducer);
