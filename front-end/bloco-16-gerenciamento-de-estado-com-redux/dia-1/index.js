a6ee3bf146bed95cba9543024e33edd05ad9b919
Michelle <mls.srbek@gmail.com>
2021-07-03 22:48:34 -0300

    lecture 16.1 - freecodecamp exercise 3

--- front-end/bloco-16-gerenciamento-de-estado-com-redux/dia-1/index.js
+++ front-end/bloco-16-gerenciamento-de-estado-com-redux/dia-1/index.js
@@ -23,3 +23,26 @@ const loginAction = () => {
 };
 
 store.dispatch(loginAction());
+
+// The code editor has the previous example as well as the start of a reducer function for you. Fill in the body of the reducer function so that if it receives an action of type 'LOGIN' it returns a state object with login set to true. Otherwise, it returns the current state. Note that the current state and the dispatched action are passed to the reducer, so you can access the action's type directly with action.type.
+
+const defaultState = {
+    login: false
+};
+  
+const reducer = (state = defaultState, action) => {
+    switch (action.type) {
+      case 'LOGIN':
+        return { login: true };
+      default: 
+        return state;
+    }
+};
+  
+const store = Redux.createStore(reducer);
+  
+const loginAction = () => {
+    return {
+      type: 'LOGIN'
+    }
+};
