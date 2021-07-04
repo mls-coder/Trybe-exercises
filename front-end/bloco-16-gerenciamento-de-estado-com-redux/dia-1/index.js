32f11467593373992f68d69e0c737c58b8c51f47
Michelle <mls.srbek@gmail.com>
2021-07-03 22:37:45 -0300

    lecture 16.1 - freecodecamp exercise 1

---
+++ front-end/bloco-16-gerenciamento-de-estado-com-redux/dia-1/index.js
@@ -0,0 +1,11 @@
+// introdução ao redux - exercicios codecamp
+
+// Define a function named actionCreator() that returns the action object when called.
+
+const action = {
+    type: 'LOGIN',
+};
+  
+function actionCreator() {
+    return action;
+}
