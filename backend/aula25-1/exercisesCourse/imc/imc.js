c38cc67fc8c118c1701e1447755b223f77ba8a52
Michelle <mls.srbek@gmail.com>
2021-10-08 10:58:59 -0300

    imc and situation

--- backend/aula25-1/exercisesCourse/imc/imc.js
+++ backend/aula25-1/exercisesCourse/imc/imc.js
@@ -5,4 +5,13 @@ const altura = readline.questionFloat('How tall are you? ');
 
 const imc = peso / (altura**2);
 
-console.log(imc);
+const situation = (imc) => {
+    if(imc < 18.5) return 'Abaixo do peso';
+    else if (imc >= 18.5 && imc <= 24.9) return 'Peso normal';
+    else if (imc >= 25 && imc <= 29.9) return 'Sobrepeso';
+    else if (imc >= 30 && imc <= 34.9) return 'Obesidade grau I';
+    else if (imc >= 35 && imc <= 39.9) return 'Obesidade grau II';
+    else return 'Obesidade grau III e IV';
+};
+
+console.log(`imc: ${imc} situação: ${situation(imc)}`);
