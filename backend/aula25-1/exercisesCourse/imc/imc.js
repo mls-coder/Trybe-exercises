816752e0e0f3718a323908acbf2df54f33826682
Michelle <mls.srbek@gmail.com>
2021-10-08 10:49:10 -0300

    readline-sync

--- backend/aula25-1/exercisesCourse/imc/imc.js
+++ backend/aula25-1/exercisesCourse/imc/imc.js
@@ -1,6 +1,7 @@
+const readline = require('readline-sync');
 
-const peso = 65;
-const altura = 1.73;
+const peso = readline.questionFloat('What\'s your weight? ');
+const altura = readline.questionFloat('How tall are you? ');
 
 const imc = peso / (altura**2);
 
